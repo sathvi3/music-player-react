c6baa0172ff744336e9d7aa554cbcbb2759749d6
sathvi3 <sathvikbhandar007@hotmail.com>
2020-11-12 17:01:20 +0530

    Added styling for Song and Player Components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
+import styles from '../src/styles/app.scss';
 
 function App() {
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Song />
       <Player />
     </div>
