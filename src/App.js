ccea2644dd4e98dcd4d6441a3a0ccd74ae45b95e
sathvi3 <sathvikbhandar007@hotmail.com>
2020-11-12 17:36:43 +0530

    Added song data and imported it in App Component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './util';
 import styles from '../src/styles/app.scss';
 
 function App() {
